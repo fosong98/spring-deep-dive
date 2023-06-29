d18cb362374c46dc2379b6c364140c1d9027ed50
fosong98 <fosong98@gmail.com>
2023-06-29 23:07:17 +0900

    bookmark

---
+++ toby2Boot/src/main/java/com/example/toby2boot/Toby2BootApplication.java
@@ -0,0 +1,13 @@
+package com.example.toby2boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Toby2BootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Toby2BootApplication.class, args);
+    }
+
+}
---
+++ toby2Boot/src/test/java/com/example/toby2boot/Toby2BootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toby2boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Toby2BootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
